fdc44cd24e3ea9cc023ef96efdb0688113583188
Jerome Vonk <lelovv@gmail.com>
2023-10-19 16:40:46 -0300

    change how data is created

--- your_data_in_the_spotlight/src/create-samples-to-csv-file.js
+++ your_data_in_the_spotlight/src/create-samples-to-csv-file.js
@@ -1,26 +1,39 @@
 const { getCsvHeader, convertToCsvRecord} = require('./util');
-const { createSampleRecord } = require('./sample-record');
+const { createDeveloper, createSampleRecord } = require('./sample-record');
 
 const fs = require('fs')
 
-let records = 10273;
+let developerCount = 759;
+console.log(`Creating ${developerCount} developers`);
+const developers = [];
 
+while (developerCount-- > 0) {
+  developers.push(createDeveloper());
+}
+
+// ---------------------------------------------------
+// Create output file
+// ---------------------------------------------------
 const FILENAME = `data/developer_records.csv`;
 const logger = fs.createWriteStream(FILENAME, {
-  flags: 'w' //  Open file for writing. The file is created (if it does not exist) or truncated (if it exists).
+  flags: 'w' //  Open file for writing. Fle is created or truncated
 })
 
 // Write the CSV header
 logger.write(`${getCsvHeader()}`);
 
-console.log(`Creating ${FILENAME} with ${records} records`);
+console.log(`Creating ${FILENAME} with ${developers.length * 12} records`);
 
-while (records-- > 0 ) {
-  const objData = createSampleRecord();
-  const csvData = convertToCsvRecord(objData)
-  
-  logger.write('\n');
-  logger.write(csvData);
+// For every month
+for (let month = 1; month <= 12; month++) {
+  // Create record for every developer
+  for (const developer of developers) {
+    const objData = createSampleRecord(developer, month);
+    const csvData = convertToCsvRecord(objData)
+    
+    logger.write('\n');
+    logger.write(csvData);
+  }
 }
 
 logger.end();
--- your_data_in_the_spotlight/src/sample-record.js
+++ your_data_in_the_spotlight/src/sample-record.js
@@ -1,13 +1,15 @@
 const { faker } = require('@faker-js/faker');
 
-exports.createSampleRecord = () => {
-
-  // Create some data that will be used to create other meaningful data
+exports.createDeveloper = () => {
   const firstName = faker.person.firstName();
   const middleName = faker.person.middleName();
   const lastName = faker.person.lastName();
 
-  // Another options is faker.location.country()
+  const software_stack = faker.helpers.weightedArrayElement([
+    { weight: 5, value: "Backend" }, { weight: 4, value: "DevOps" },
+    { weight: 7, value: "Data Engineer" }, { weight: 9, value: "Frontend" },
+  ]);
+
   const country = faker.helpers.weightedArrayElement([
     { weight: 5, value: "Chile" }, { weight: 21, value: "Brazil" },
     { weight: 7, value: "Sweden" }, { weight: 4, value: "Nigeria" },
@@ -20,20 +22,21 @@ exports.createSampleRecord = () => {
     { weight: 10, value: "Uruguay" }, { weight: 1, value: "Portugal" },
   ]);
 
-  const software_stack = faker.helpers.weightedArrayElement([
-    { weight: 5, value: "Backend" }, { weight: 4, value: "DevOps" },
-    { weight: 7, value: "Data Engineer" }, { weight: 9, value: "Frontend" },
-  ]);
 
-  const randomMonth = faker.number.int({ min: 1, max: 12 });
-
-  const fakeData = {
+  return {
     name: `${firstName} ${middleName} ${lastName}`,
     country,
-    rate: faker.number.int({ min: 14, max: 28 }),
-    date: `${randomMonth.toString().padStart(2, '0')}-2023`,
+    rate: faker.number.int({ min: 18, max: 24 }),
+    software_stack
+  }
+}
+
+exports.createSampleRecord = (developer, month) => {
+
+  const fakeData = {
+    ...developer,
+    date: `${month.toString().padStart(2, '0')}-2023`,
     hours_worked: faker.number.int({ min: 80, max: 160 }),
-    software_stack,
   };
 
   // -------------------------------------------------------------
@@ -46,15 +49,15 @@ exports.createSampleRecord = () => {
   }
 
   // The greater the month, the greater the hours worked
-  fakeData.hours_worked = Math.floor(fakeData.hours_worked * (1 + (randomMonth / 50)));
+  fakeData.hours_worked = Math.floor(fakeData.hours_worked * (1 + (month / 50)));
 
   // Except for May, when people worked less
-  if (randomMonth == 5) {
+  if (month == 5) {
     fakeData.hours_worked = Math.floor(fakeData.hours_worked * 0.7);
   }
 
   // And September, when people worked much more!
-  if (randomMonth == 9) {
+  if (month == 9) {
     fakeData.hours_worked = Math.floor(fakeData.hours_worked * 1.1);
   }
 
